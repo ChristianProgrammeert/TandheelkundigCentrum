c5e023a6974fbefdd6b6c630f480bc12da14b950
hilgenkampc <christianhilgenkamp@gmail.com>
2024-03-11 09:57:01 +0100

    fix: decimal to double

--- TandheelkundigCentrum/Data/Models/Treatment.cs
+++ TandheelkundigCentrum/Data/Models/Treatment.cs
@@ -10,7 +10,7 @@ public class Treatment : IBaseEntity<int>
     [Key] public int Id { get; set; }
     public string Name { get; set; }
     public TimeSpan Duration { get; set; }
-    public decimal Price { get; set; }
+    public double Price { get; set; }
     public bool Archived { get; set; }
     public Collection<Rating> Ratings { get; internal set; }
 }
