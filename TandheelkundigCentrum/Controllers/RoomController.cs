b03ccccb6dfe2c63eca0134ae67617a37f83f40d
Dimphy Hilbrink <0328455@student.rocvantwente.nl>
2024-03-11 12:01:09 +0100

    added services and base code for the rooms

---
+++ TandheelkundigCentrum/Controllers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TandheelkundigCentrum.Data;
+using TandheelkundigCentrum.Data.Models;
+using TandheelkundigCentrum.Services;
+
+namespace TandheelkundigCentrum.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly RoomService roomService;
+        public RoomController(ApplicationDbContext context) { 
+            roomService = new RoomService(context);
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var rooms = await roomService.GetAllAsync();
+            return View(rooms);
+        }
+    }
+}
---
+++ TandheelkundigCentrum/Services/RoomService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TandheelkundigCentrum.Data.Base;
+using TandheelkundigCentrum.Data.Models;
+using TandheelkundigCentrum.Data;
+
+namespace TandheelkundigCentrum.Services;
+
+public class RoomService(ApplicationDbContext context) : BaseRepository<Room, int>(context)
+{
+
+}
