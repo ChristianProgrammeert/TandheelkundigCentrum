ba63ce18fd5c14d303047e46f2e82844859968b8
hilgenkampc <christianhilgenkamp@gmail.com>
2024-03-19 00:33:46 +0100

    Initial Commit

---
+++ TandheelkundigCentrum/Controllers/TreatmentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TandheelkundigCentrum.Data;
+using TandheelkundigCentrum.Data.Models;
+using TandheelkundigCentrum.Filters;
+using TandheelkundigCentrum.Services;
+
+namespace TandheelkundigCentrum.Controllers
+{
+    [AuthFilter(Group.GroupName.Admin, Group.GroupName.Assistent, Group.GroupName.Dentist)]
+    public class TreatmentController : Controller
+    {
+        private readonly TreatmentService treatmentService;
+        public TreatmentController(ApplicationDbContext context) { 
+            treatmentService = new TreatmentService(context);
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var rooms = await treatmentService.GetAllAsync();
+            return View(rooms);
+        }
+
+        /// <summary>
+        /// View edit page with the treatment with the given id, if id is given.
+        /// </summary>
+        public async Task<IActionResult> Edit(int? treatmentId)
+        {
+            Treatment? treatment = treatmentId == null
+                ? null
+                : await treatmentService.GetByIdAsync(treatmentId.Value);
+            return View(treatment);
+        }
+
+        /// <summary>
+        /// When an edit is done, add it to the service when id is not set, else update the treatment with the given id.
+        /// Redirect to the view page of the new or updated treatment.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, Treatment treatment)
+        {
+            treatment = id == null
+                ? await treatmentService.AddAsync(treatment)
+                : await treatmentService.UpdateAsync(treatment);
+            return RedirectToAction("View", new { id = treatment.Id });
+        }
+
+        /// <summary>
+        /// Delete the treatment with the given id and redirect to the index view
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Delete(int id)
+        {
+            await treatmentService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// View view page with the treatment with the given id.
+        /// </summary>
+        public async Task<ViewResult> View(int id)
+        {
+            return View(await treatmentService.GetTreatment(id));
+        }
+    }
+}
---
+++ TandheelkundigCentrum/Services/TreatmentService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TandheelkundigCentrum.Data.Base;
+using TandheelkundigCentrum.Data.Models;
+using TandheelkundigCentrum.Data;
+
+namespace TandheelkundigCentrum.Services;
+
+public class TreatmentService(ApplicationDbContext context) : BaseRepository<Treatment, int>(context)
+{
+    /// <summary>
+    /// Get the treatment with the given id
+    /// </summary>
+    public async Task<Treatment?> GetTreatment(int id)
+    {
+        return await GetByIdAsync(id);
+    }
+}
