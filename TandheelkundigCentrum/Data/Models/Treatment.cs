a7db7ab1dfb346b9382cfdaf7ad68c481e9a4fe0
Dimphy Hilbrink <0328455@student.rocvantwente.nl>
2024-03-19 15:42:10 +0100

    fixed format of time in treatments and updated the styling of the treatments pages

--- TandheelkundigCentrum/Data/Models/Treatment.cs
+++ TandheelkundigCentrum/Data/Models/Treatment.cs
@@ -9,7 +9,7 @@ public class Treatment : IBaseEntity<int>
 {
     [Key] public int Id { get; set; }
     public string Name { get; set; }
-    public TimeSpan Duration { get; set; }
+    public int Duration { get; set; }
     public double Price { get; set; }
     public bool Archived { get; set; }
     public Collection<Rating> Ratings { get; internal set; }
