33b236e431a4ff82488a43610943593fa7cdab88
leslmosnk <leslmosnk@outlook.com>
2024-03-20 11:55:45 +0100

    treatments now get displayed in apointments index

--- TandheelkundigCentrum/Controllers/AppointmentController.cs
+++ TandheelkundigCentrum/Controllers/AppointmentController.cs
@@ -24,7 +24,7 @@ namespace TandheelkundigCentrum.Controllers
 
             List<Appointment> appointments;
             if (group.Contains(Group.GroupName.Assistent))
-                appointments = (await appointmentService.GetAllAsync(appointment => appointment.Room, a => a.Dentist, a => a.Patient)).ToList();
+                appointments = (await appointmentService.GetAllAsync(appointment => appointment.Room, a => a.Dentist, a => a.Patient, a => a.Treatments)).ToList();
             else
                 appointments = await appointmentService.GetAppointmentByUser(id);
             return View(appointments);
